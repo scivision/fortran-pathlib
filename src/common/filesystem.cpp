805ea7e4c1c48b0971a82e03221bd084266959ca
scivision <scivision@users.noreply.github.com>
2024-04-05 01:37:09 -0400

    cpp:macos: mkdir needs fallback due to stdlib bug on macos appleclang

--- src/common/filesystem.cpp
+++ src/common/filesystem.cpp
@@ -57,7 +57,7 @@ bool Ffs::mkdir(std::string_view path)
 
   std::error_code ec;
   // old MacOS return false even if directory was created
-  if (std::filesystem::create_directories(path, ec) && !ec && Ffs::is_dir(path)) FFS_LIKELY
+  if (std::filesystem::create_directories(path, ec) || (!ec && Ffs::is_dir(path))) FFS_LIKELY
     return true;
 
   std::cerr << "ERROR:ffilesystem:mkdir: " << ec.message() << "\n";
