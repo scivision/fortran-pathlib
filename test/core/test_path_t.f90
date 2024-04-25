f68fed1cb635e7ac0cc2f49b0cd86093bfc1d673
scivision <scivision@users.noreply.github.com>
2024-04-25 00:12:27 -0400

    test:path_t more error msg

--- test/core/test_path_t.f90
+++ test/core/test_path_t.f90
@@ -81,53 +81,57 @@ call get_command_argument(0, s2, status=i)
 if (i /= 0) error stop "ERROR: get_command_argument"
 
 p1 = path_t(s2)
-if (.not. p1%exists()) error stop "exists(file)"
-if (p1%is_dir()) error stop "is_dir(file)"
-if (.not. p1%is_file()) error stop "is_file(file)"
+if (.not. p1%exists()) error stop "ERROR:test: exists(file)"
+if (p1%is_dir()) error stop "ERROR:test: is_dir(file)"
+if (.not. p1%is_file()) error stop "ERROR:test: is_file(file)"
 p1 = path_t(p1%parent())
-if (.not. p1%is_dir()) error stop "is_dir(dir)"
+if (.not. p1%is_dir()) error stop "ERROR:test: is_dir(dir)"
 
 !> is_exe
 p1 = path_t(s2)
-if (.not. p1%is_exe()) error stop "is_exe(exe)"
+if (.not. p1%is_exe()) error stop "ERROR:test: is_exe(exe)"
 p1 = path_t(p1%parent())
-if (p1%is_exe()) error stop "is_exe(dir)"
+if (p1%is_exe()) error stop "ERROR:test: is_exe(dir)"
 
 !> file_size
 p1 = path_t(s2)
-if (p1%file_size() == 0) error stop "file_size"
+if (p1%file_size() == 0) error stop "ERROR:test: file_size"
 
 !> space_available
 p1 = path_t(s2)
-if (p1%space_available() == 0) error stop "space_available"
+i64 = p1%space_available()
+if (i64 == 0) then
+  write(stderr, '(a,i0)') "ERROR:test: space_available: " // p1%root() // ' ', i64
+  error stop
+endif
 
 !> modtime
 i64 = p1%modtime()
-if (i64 == 0) error stop "modtime"
+if (i64 == 0) error stop "ERROR:test: modtime"
 
 !> get_permissions
 c9 = p1%get_permissions()
 print '(a)', "get_permissions: " // c9
-if(c9(1:3) /= "rwx") error stop "get_permissions expected user rwx"
+if(c9(1:3) /= "rwx") error stop "ERROR:test: get_permissions expected user rwx"
 
 !> is_subdir
 p1 = path_t("a/b")
-if (.not.p1%is_subdir("a")) error stop "is_subdir(a)"
-if (p1%is_subdir("a/b/c")) error stop "is_subdir(a/b/c)"
+if (.not.p1%is_subdir("a")) error stop "ERROR:test: is_subdir(a)"
+if (p1%is_subdir("a/b/c")) error stop "ERROR:test: is_subdir(a/b/c)"
 
 !> join
 p1 = path_t("a")
 p1 = p1%join("b")
-if (p1%path() /= "a/b") error stop "join"
+if (p1%path() /= "a/b") error stop "ERROR:test: join"
 
 !> normal
 p1 = path_t("a/b/../c")
 p1 = p1%normal()
-if (p1%path() /= "a/c") error stop "normal"
+if (p1%path() /= "a/c") error stop "ERROR:test: normal"
 
 !> parent
 p1 = path_t("a/b/c")
-if (p1%parent() /= "a/b") error stop "parent"
+if (p1%parent() /= "a/b") error stop "ERROR:test: parent"
 
 !> proximate
 if(is_windows()) then
@@ -136,24 +140,24 @@ else
   p1 = path_t("/a/b")
 endif
 s1 = p1%proximate_to("c")
-if (s1 /= "c") error stop "proximate_to: " // s1
+if (s1 /= "c") error stop "ERROR:test: proximate_to: " // s1
 
 !> relative
 s1 = p1%relative_to("c")
-if (s1 /= "") error stop "relative_to: " //s1
+if (s1 /= "") error stop "ERROR:test: relative_to: " //s1
 
 !> root
 p1 = path_t("/c/b")
-if (p1%root() /= "/") error stop "root: " // p1%root()
+if (p1%root() /= "/") error stop "ERROR:test: root: " // p1%root()
 
 !> stem
 p1 = path_t("a/b.txt")
-if (p1%stem() /= "b") error stop "stem: " // p1%stem()
+if (p1%stem() /= "b") error stop "ERROR:test: stem: " // p1%stem()
 
 !> suffix
 s1 = p1%suffix()
 if (s1 /= ".txt") then
-  write(stderr,'(a)') "ERROR: suffix: " // s1 // " /= .txt"
+  write(stderr,'(a)') "ERROR:test: suffix: " // s1 // " /= .txt"
   error stop
 endif
 print '(a)', "PASS: %suffix: " // s1
@@ -165,13 +169,13 @@ if (.not. p1%is_file()) error stop "touch"
 
 !> copy_file, with_suffix
 p2 = p1%with_suffix(".dat")
-if(p2%path() /= "touch.dat") error stop "with_suffix: " // p2%path()
+if(p2%path() /= "touch.dat") error stop "ERROR:test: with_suffix: " // p2%path()
 call p1%copy_file(p2%path(), overwrite=.true.)
-if (.not. p2%is_file()) error stop "copy_file"
+if (.not. p2%is_file()) error stop "ERROR:test: copy_file"
 
 !> same_file, remove
 p2 = path_t("./touch.txt")
-if (.not. p1%same_file(p2)) error stop "same_file"
+if (.not. p1%same_file(p2)) error stop "ERROR:test: same_file"
 call p2%remove()
 
 !> is_reserved, is_char_device
@@ -182,7 +186,7 @@ b = p1%is_char_device()
 !> mkdir
 p1 = path_t("test_dir")
 call p1%mkdir(ok=b)
-if (.not. b) error stop "mkdir"
+if (.not. b) error stop "ERROR:test: mkdir"
 
 !> is_symlink, create_symlink, read_symlink
 p1 = path_t("touch.txt")
@@ -192,11 +196,11 @@ p2 = path_t("touch.link")
 if(p2%is_file()) call p2%remove()
 
 call p1%create_symlink(p2%path())
-if (.not. p2%is_symlink()) error stop "create_symlink: " // p2%path()
+if (.not. p2%is_symlink()) error stop "ERROR:test: create_symlink: " // p2%path()
 
 if (.not. is_windows()) then
 p2 = p2%read_symlink()
-if (p2%path() /= p1%path()) error stop "read_symlink " // p2%path()
+if (p2%path() /= p1%path()) error stop "ERROR:test: read_symlink " // p2%path()
 endif
 
 !> cleanup
