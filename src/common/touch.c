5f80d9bca3552a4a7431f25d386c801571f559e5
scivision <scivision@users.noreply.github.com>
2024-06-16 19:40:33 -0400

    touch: more detail error messages

--- src/common/touch.c
+++ src/common/touch.c
@@ -12,9 +12,20 @@ bool fs_touch(const char* path)
     return fs_set_modtime(path);
 
   FILE* fid = fopen(path, "a+b");
-  if(fid && fclose(fid) == 0)
-    return true;
+  if(!fid){
+    fprintf(stderr, "ERROR:ffilesystem:touch:fopen %s => %s\n", path, strerror(errno));
+    return false;
+  }
 
-  fprintf(stderr, "ERROR:Ffilesystem:touch: %s => %s\n", path, strerror(errno));
+  if(fclose(fid) != 0){
+    fprintf(stderr, "ERROR:ffilesystem:touch:fclose %s => %s\n", path, strerror(errno));
+    return false;
+  }
+
+  if(fs_is_file(path))
+    return true;
+  // is_file check because sometimes fclose() doesn't fail, but the file is not created
+
+  fprintf(stderr, "ERROR:Ffilesystem:touch: %s was not created\n", path);
   return false;
 }
