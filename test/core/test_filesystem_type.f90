1d8abf038a09b2acddb7613f9048af4545315ab7
scivision <scivision@users.noreply.github.com>
2024-07-30 17:26:06 -0400

    test:filesystem_type: skip if unknown filesystem

--- test/core/test_filesystem_type.f90
+++ test/core/test_filesystem_type.f90
@@ -8,7 +8,6 @@ implicit none
 
 valgrind : block
 
-character(:), allocatable :: exp(:)
 character(:), allocatable :: fsname
 
 if(is_cygwin()) then
@@ -17,27 +16,13 @@ else
   fsname = filesystem_type(".")
 end if
 
-print '(a)', fsname
-
-if(is_windows() .or. is_cygwin()) then
-  exp = [character(5) :: "NTFS", "EXFAT"]
-elseif (is_wsl() > 0) then
-  exp = [character(4) :: "ext4", "nfs", "v9fs"]
-elseif (is_linux()) then
-  exp = [character(5) :: "ext4", "nfs", "tmpfs", "xfs"]
-elseif (is_macos()) then
-  exp = [character(4) :: "apfs", "hfs"]
-else
+if(len_trim(fsname) == 0) then
+  write(stderr, '(a)') "Unknown filesystem type, see type ID in stderr to update fs_get_type()"
   error stop 77
-end if
-
-if (.not. any(fsname == exp)) then
-  write(stderr, *) "FAIL: got " // fsname // " but expected: ", exp
-  error stop
 endif
 
-end block valgrind
+print '(a)', "OK: filesystem_type: " // fsname
 
-print '(a)', "OK: filesystem_type"
+end block valgrind
 
 end program
