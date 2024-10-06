b54a3a685c0c695ece7747bad94de81e10f243af
scivision <scivision@users.noreply.github.com>
2024-10-06 12:35:55 -0400

    test:which: enhance feedback if not in cwd

--- test/executable/test_which.f90
+++ test/executable/test_which.f90
@@ -52,19 +52,27 @@ if(which("") /= "") error stop "ERROR:test_exe: which(empty) failed"
 s2 = getarg(0)
 if(len_trim(s2) == 0) error stop "could not get own program name"
 s1 = "./" // file_name(s2)
+if(.not. is_file(s1)) error stop "ERROR:test_exe: file not found: " // s1
 s3 = which(s1)
 print '(a)', "which(" // s1 // ") = " // s3
-if(len_trim(s3) == 0) error stop "ERROR:test_exe: which(relative) failed"
+if(len_trim(s3) == 0) then
+  write(stderr, '(a,i0)') "ERROR:test_exe: which(relative) failed: " // s3, len_trim(s3)
+  error stop "ERROR:test_exe: which(relative) failed"
+endif
 
 !> relative path not exist parent path (should be empty)
 s1 = "not-exist/" // file_name(s2)
 s3 = which(s1)
 print '(a)', "which(" // s1 // ") = " // s3
-if(len_trim(s3) /= 0) error stop "ERROR:test_exe: which(relative_not_exist) failed"
+if(len_trim(s3) /= 0) then
+  write(stderr,'(a,i0)') "ERROR:test_exe: which(relative_not_exist) failed: " // s3, len_trim(s3)
+  error stop "ERROR:test_exe: which(relative_not_exist) failed"
+endif
 
 !> cwd priority on Windows only
 if(is_windows()) then
   s3 = file_name(s2)
+  if (.not. is_file(s3)) error stop "ERROR:test_exe: file not found--is test running in exe directory: " // s3
   s1 = which(s3)
   print '(a)', "cwd: which(" // s3 // ")" // " = " // s1
   if (len_trim(s1) == 0) error stop "ERROR:test_exe: which(cwd) failed"
