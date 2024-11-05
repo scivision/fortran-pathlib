fc67291dc33f78d52a1919c0c1a67c9d5076364a
scivision <scivision@users.noreply.github.com>
2024-11-05 13:04:28 -0500

    test: add hard_link_count

---
+++ test/core/test_hard_link.cpp
@@ -0,0 +1,21 @@
+#include "ffilesystem.h"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main() {
+
+  uintmax_t c = fs_hard_link_count(".");
+
+  if(c < 1){
+    std::cerr << "ERROR: hard_link_count: " << c << "\n";
+    return EXIT_FAILURE;
+  }
+
+
+
+  std::cout << "OK: hard_link_count(.) " << c << "\n";
+
+  return EXIT_SUCCESS;
+}
