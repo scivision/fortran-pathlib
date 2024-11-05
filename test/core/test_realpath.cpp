ebc860bc131067ae5169d47e1a9edecc112f5f32
scivision <scivision@users.noreply.github.com>
2024-11-05 12:38:35 -0500

    test:realpath: Cygwin empty if not exist

--- test/core/test_realpath.cpp
+++ test/core/test_realpath.cpp
@@ -13,27 +13,44 @@ auto cwd = fs_get_cwd();
 if(cwd.empty())
   err("current working dir not found");
 
-std::string r = fs_realpath(".");
+
+std::cout << "cwd: " << cwd << " length " << cwd.length() << "\n";
+
+int e = 0;
+
+const std::string r = fs_realpath(".");
 if (r != cwd){
   std::cerr << "FAILED: fs_realpath(.) " << r << " != " << cwd << "\n";
-  std::cerr << "r: " << r << " length " << r.length() << "\n";
-  std::cerr << "cwd: " << cwd << " length " << cwd.length() << "\n";
-  err("fs_realpath(.)");
-}
-std::cout << "fs_realpath(.) " << r << "\n";
+  std::cerr << r << " length " << r.length() << "\n";
+  e++;
+} else
+  std::cout << "fs_realpath(.) " << r << "\n";
 
+const std::string name = "not-exist-realpath";
+const std::string not_exist = cwd + "/" + name;
+const std::string rt = fs_realpath(name);
+if(rt != not_exist){
+  if(fs_is_cygwin()){
+    std::cerr << "On Cygwin, realpath() returns empty string for non-existing file\n";
+  } else {
+    std::cerr << "FAILED: fs_realpath(test) " << rt << " != " << not_exist << "\n";
+    std::cerr << rt << " length " << rt.length() << "\n";
+    e++;
+  }
+} else
+  std::cout << "fs_realpath(test) " << rt << "\n";
 
-r = fs_realpath("test");
-if(r != cwd + "/test"){
-  std::cerr << "FAILED: fs_realpath(test) " << r << " != " << cwd << "/test\n";
-  std::cerr << "CWD: " << cwd << "\n";
-  err("fs_realpath(test)");
-}
+const std::string p = fs_realpath("..");
+if (p.length() >= r.length()){
+  std::cerr << "FAILED: fs_realpath(..) " << p << " >= " << r << "\n";
+  std::cerr << p << " length " << p.length() << "\n";
+  std::cerr << r << " length " << r.length() << "\n";
+  e++;
+} else
+  std::cout << "fs_realpath(..) " << p << " < " << r << "\n";
 
-std::string p = fs_realpath("..");
-if (p.length() >= r.length())
-  err("fs_realpath(..) " + p + " >= " + r);
-std::cout << "fs_realpath(..) " << p << " < " << r << "\n";
+if(e)
+  err("fs_realpath failed");
 
 std::cout << "OK: C++ realpath\n";
 
--- test/core/test_realpath.f90
+++ test/core/test_realpath.f90
@@ -6,18 +6,11 @@ use filesystem
 
 implicit none
 
-
-call test_realpath()
-print '(a)', "OK: realpath full"
-
-contains
-
-subroutine test_realpath()
+valgrind : block
 
 character(:), allocatable :: p1, p2
-character(*), parameter :: dummy = "nobody.txt"
 
-integer :: L1, L2, L3
+integer :: L1, L2
 
 ! -- current directory  -- old MacOS doesn't handle "." or ".." alone
 p1 = realpath(".")
@@ -44,43 +37,7 @@ if (L2 >= L1) then
 end if
 print *, 'OK: realpath(..) = ', p1
 
-! -- relative, non-existing file
-p2 = '../' // dummy
-p1 = realpath(p2)
-
-L3 = len_trim(p1)
-if(L3 == 0) then
-  if(is_cygwin()) then
-    write(stderr,'(a)') "On Cygwin, realpath() returns empty string for non-existing file"
-  else
-    error stop "FAILED: relative file: realpath(" // p2 // ") => " // p1
-  endif
-endif
-
-if(.not. is_cygwin()) then
-  L1 = len(dummy)
-  if (L2 > 1) L1 = L1 + 1  !< when L2==1, $HOME is like /root instead of /home/user
-
-  if (L3 - L2 /= L1) then
-    write(stderr,*) 'ERROR relative file was not realpath: ' // p1, L2, L3, len(dummy)
-    error stop
-  end if
-endif
-
-!> not exist
-p1 = realpath("not-exist/dir/..")
-p2 = get_cwd() // "/not-exist"
-
-print '(a)', "realpath(not-exist/dir/..) = ", p1
-
-if (p1 /= p2) then
-  write(stderr,*) 'ERROR: relative not-exist dir did not realpath: ' // p1 // " /= " // p2
-  if(.not. is_cygwin()) error stop
-end if
-
-print *, 'OK: canon_file = ', p1
-
-end subroutine test_realpath
+end block valgrind
 
 
 end program
