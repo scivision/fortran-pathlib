a7e209e1bcab739078f9ad65373109675420ae47
scivision <scivision@users.noreply.github.com>
2024-11-08 14:13:10 -0500

    setenv:windows: use _putenv_s

--- src/env.cpp
+++ src/env.cpp
@@ -1,15 +1,12 @@
-#ifdef _MSC_VER
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-#ifndef _CRT_NONSTDC_NO_WARNINGS
-#define _CRT_NONSTDC_NO_WARNINGS
-#endif
-#endif
-
 #if defined(__linux__) && !defined(_DEFAULT_SOURCE)
 #define _DEFAULT_SOURCE
 #endif
+
+#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+
 
 #include <cstdlib> // putenv, setenv
 #include <string>
@@ -41,8 +38,7 @@ bool fs_setenv(std::string_view name, std::string_view value)
   // SetEnvironmentVariable returned OK but set blank values
   // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/putenv-wputenv
 
-  if(std::string v = std::string(name) + "=" + std::string(value);
-      putenv(v.data()) == 0)
+  if(_putenv_s(name.data(), value.data()) == 0)
     return true;
 #else
   // https://www.man7.org/linux/man-pages/man3/setenv.3.html
