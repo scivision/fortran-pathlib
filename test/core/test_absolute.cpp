184d4c6ce2273e29aa8afab0b239467342c8ecf2
scivision <scivision@users.noreply.github.com>
2024-11-20 16:12:44 -0500

    test:absolute:c++

---
+++ test/core/test_absolute.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "ffilesystem.h"
+#include "ffilesystem_test.h"
+#include <cstdlib>
+
+int main() {
+    std::string in = "rel";
+    std::string base, ref, out;
+
+    if (fs_is_windows()) {
+        base = "j:/foo";
+        ref = "j:/foo/rel";
+    } else {
+        base = "/foo";
+        ref = "/foo/rel";
+    }
+
+    out = fs_absolute(in, base, false);
+    if (out.empty())
+        err("absolute() has empty output");
+    if (out != ref)
+        err("Mismatch: absolute(" + in + ", " + base + ") " + out + " != " + ref);
+
+    ref = fs_get_cwd() + "/rel";
+    out = fs_absolute(in, false);
+    if (out.empty())
+        err("absolute(" + in + ", '') has empty output");
+    if (out != ref)
+        err("absolute(" + in + ", '') = " + out + " != " + ref);
+
+    ref = fs_get_cwd();
+    out = fs_absolute("", false);
+    if (out.empty())
+        err("absolute('', '') has empty output");
+    if (out != ref)
+        err("absolute('', ''): " + out + " != " + ref);
+
+    ref = fs_get_cwd() + "/rel";
+    out = fs_absolute("", in, false);
+    if (out.empty())
+        err("absolute('', path) has empty output");
+    if (out != ref)
+        err("absolute('', "  + in + "): " + out + " != " + ref);
+
+    std::cout << "OK: absolute C++\n";
+
+    return EXIT_SUCCESS;
+}
