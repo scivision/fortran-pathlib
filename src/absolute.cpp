f1c46670a894a388b549f148340b2531188f9957
scivision <scivision@users.noreply.github.com>
2024-11-21 15:19:43 -0500

    absolute: simplify

--- src/absolute.cpp
+++ src/absolute.cpp
@@ -16,25 +16,16 @@ std::string fs_absolute(std::string_view path, const bool expand_tilde)
   // Inspired by Python pathlib.Path.absolute()
   // https://docs.python.org/3/library/pathlib.html#pathlib.Path.absolute
 
-#ifdef HAVE_CXX_FILESYSTEM
-  const std::filesystem::path ex = expand_tilde
-    ? std::filesystem::path(fs_expanduser(path))
-    : path;
-
-  if (ex.is_absolute())
-    return ex.generic_string();
-#else
   const std::string ex = expand_tilde
     ? fs_expanduser(path)
     : std::string(path);
 
-  if (fs_is_absolute(ex))
-    return ex;
-#endif
-
   // Linux, MinGW can't handle empty paths
   if(ex.empty())
     return fs_get_cwd();
+
+  if (fs_is_absolute(ex))
+    return ex;
 
 #ifdef HAVE_CXX_FILESYSTEM
   std::error_code ec;
@@ -47,13 +38,14 @@ std::string fs_absolute(std::string_view path, const bool expand_tilde)
   fs_print_error(path, "absolute", ec);
   return {};
 #else
-  const auto cwd = fs_get_cwd();
+  std::string cwd = fs_get_cwd();
   if(cwd.empty())
     return {};
 
-  return (cwd.back() != '/')
-    ? cwd + '/' + ex
-    : cwd + ex;
+  if(cwd.back() != '/')
+    cwd += '/';
+
+  return cwd + ex;
 #endif
 }
 
@@ -63,21 +55,12 @@ std::string fs_absolute(std::string_view path, std::string_view base, const bool
 {
   // rebase path on base.
 
-#ifdef HAVE_CXX_FILESYSTEM
-  const std::filesystem::path ex = expand_tilde
-    ? std::filesystem::path(fs_expanduser(path))
-    : path;
-
-  if (ex.is_absolute())
-    return ex.generic_string();
-#else
   const std::string ex = expand_tilde
     ? fs_expanduser(path)
     : std::string(path);
 
   if(fs_is_absolute(ex))
     return ex;
-#endif
 
   std::string b = fs_absolute(base, expand_tilde);
   if(b.empty())
@@ -87,12 +70,5 @@ std::string fs_absolute(std::string_view path, std::string_view base, const bool
     b += '/';
 
   // don't need join(). Keeps it like Python pathlib.Path.absolute()
-  b +=
-#ifdef HAVE_CXX_FILESYSTEM
-    ex.generic_string();
-#else
-    ex;
-#endif
-
-  return b;
+  return b + ex;
 }
