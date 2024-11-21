d14378e89a6ddd60a9a2c570b73c55e1da29d854
scivision <scivision@users.noreply.github.com>
2024-11-20 23:23:22 -0500

    test:remove add UTF-8

---
+++ test/core/test_remove.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <string_view>
+
+#include "ffilesystem.h"
+#include "ffilesystem_test.h"
+
+
+int
+#ifdef _WIN32
+wmain(int argc, wchar_t* argv[])
+#else
+main(int argc, char* argv[])
+#endif
+{
+
+  if(argc < 2){
+    std::cerr << "Please specify filename to remove\n";
+    return EXIT_FAILURE;
+  }
+
+  const std::string j =
+#ifdef _WIN32
+    fs_to_narrow(argv[1]);
+#else
+  argv[1];
+#endif
+
+  std::cout << "Test file " << j << "\n";
+
+  if(!fs_is_file(j)){
+    std::cerr << "is_file(" << j << ") should be true for existing regular file\n";
+    return 77;
+  }
+
+  if(!fs_remove(j)){
+    if(fs_exists(j))
+      err("remove(" + j + ") failed--file still exists");
+    err("remove(" + j + ") failed");
+  }
+
+  std::cout << "OK: filesystem remove non-ascii.  Backend " << fs_backend() << "\n";
+
+  return EXIT_SUCCESS;
+}
