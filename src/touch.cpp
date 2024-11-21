f01b9c02d4eedb874f911f1255baa5c379663d13
scivision <scivision@users.noreply.github.com>
2024-11-20 22:24:43 -0500

    touch: simplify

--- src/touch.cpp
+++ src/touch.cpp
@@ -1,5 +1,5 @@
 #include <string_view>
-#include <cstdio> // fopen, fclose
+#include <fstream> // std::ofstream
 
 #include "ffilesystem.h"
 
@@ -7,29 +7,13 @@
 bool fs_touch(std::string_view path)
 {
   if(fs_exists(path))
-    return fs_set_modtime(path.data());
+    return fs_set_modtime(path);
 
-
-  FILE* fid = nullptr;
-#ifdef _MSC_VER
-    if (fopen_s(&fid, path.data(), "w") != 0)
-      fid = nullptr;
-#else
-    fid = fopen(path.data(), "w");
-#endif
-  if(!fid){  FFS_UNLIKELY
-    fs_print_error(path, "touch:fopen");
-    return false;
-  }
-
-  if(fclose(fid) != 0){  FFS_UNLIKELY
-    fs_print_error(path, "touch:fclose");
-    return false;
-  }
-
-  if(fs_is_file(path.data()))  FFS_LIKELY
+  std::ofstream f(path.data(), std::ios::out);
+  if(f){ FFS_LIKELY
+    f.close();
     return true;
-  // is_file check because sometimes fclose() doesn't fail, but the file is not created
+  }
 
   fs_print_error(path, "touch");
   return false;
