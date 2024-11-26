0357c94707bd7174efa128fad5890c4b5d5ae457
scivision <scivision@users.noreply.github.com>
2024-11-26 14:58:04 -0500

    is_empty: simplify

--- src/size.cpp
+++ src/size.cpp
@@ -63,94 +63,92 @@ bool fs_is_empty(std::string_view path)
   std::error_code ec;
 
   return std::filesystem::is_empty(path, ec) && !ec;
-
 #else
+
+  if(!fs_exists(path))
+    return false;
+
+  if (!fs_is_dir(path))
+    return fs_file_size(path) == 0;
 
   const std::set<std::string_view> dots = {".", ".."};
 
-  if (fs_is_dir(path))
-  {
-    if(FS_TRACE) std::cout << "TRACE: is_empty: checking directory " << path << "\n";
-    // directory empty
+  // directory empty
 #ifdef _MSC_VER
-    // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-findfirstfilea
-    WIN32_FIND_DATAA ffd;
-    HANDLE hFind = FindFirstFileA((std::string(path) + "/*").data(), &ffd);
-    if (hFind == INVALID_HANDLE_VALUE)
-    {
-      fs_print_error(path, "is_empty:FindFirstFile");
-      return false;
-    }
+  // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-findfirstfilea
+  WIN32_FIND_DATAA ffd;
+  HANDLE hFind = FindFirstFileA((std::string(path) + "/*").data(), &ffd);
+  if (hFind == INVALID_HANDLE_VALUE) {
+    fs_print_error(path, "is_empty:FindFirstFile");
+    return false;
+  }
 
-    do
-    {
-        if(FS_TRACE) std::cout << "TRACE: is_empty: do " << ffd.cFileName << "\n";
-        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-            if (
+  do
+  {
+      if(FS_TRACE) std::cout << "TRACE: is_empty: do " << ffd.cFileName << "\n";
+      if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+          if (
 #if __cplusplus >= 202002L
-            dots.contains(ffd.cFileName)
+          dots.contains(ffd.cFileName)
 #else
-            dots.find(ffd.cFileName) != dots.end()
+          dots.find(ffd.cFileName) != dots.end()
 #endif
-            )
-              continue;
-        // directory that is not . or ..
-        {
-            FindClose(hFind);
-            return false;
-        }
-        // any non-directory
-        FindClose(hFind);
-        return false;
-    } while (FindNextFileA(hFind, &ffd));
+          )
+            continue;
+      // directory that is not . or ..
+      {
+          FindClose(hFind);
+          return false;
+      }
+      // any non-directory
+      FindClose(hFind);
+      return false;
+  } while (FindNextFileA(hFind, &ffd));
 
-    // empty directory
-    FindClose(hFind);
-    return true;
+  // empty directory
+  FindClose(hFind);
+  return true;
 #else
 // https://www.man7.org/linux/man-pages/man3/opendir.3.html
 // https://www.man7.org/linux/man-pages/man3/readdir.3.html
 // https://www.man7.org/linux/man-pages/man3/closedir.3.html
 // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/readdir.3.html
 
-    if (DIR *d = opendir(path.data()); d)
-    {
-      struct dirent *entry;
-    while ((entry = readdir(d)))
-    {
+  if (DIR *d = opendir(path.data()); d)
+  {
+    struct dirent *entry;
+  while ((entry = readdir(d)))
+  {
 #ifdef _DIRENT_HAVE_D_TYPE
-      if (entry->d_type == DT_DIR)
+    if (entry->d_type == DT_DIR)
 #else
-      if (fs_is_dir(std::string(path) + "/" + entry->d_name))
+    if (fs_is_dir(std::string(path) + "/" + entry->d_name))
 #endif
-      {
+    {
 #if __cplusplus >= 202002L
-        if (dots.contains(entry->d_name))
+      if (dots.contains(entry->d_name))
 #else
-        if (dots.find(entry->d_name) != dots.end())
+      if (dots.find(entry->d_name) != dots.end())
 #endif
-          continue;
-        // directory that is not . or ..
-        closedir(d);
-        return false;
-      }
-      // any non-directory
+        continue;
+      // directory that is not . or ..
       closedir(d);
       return false;
     }
-      // empty directory
-      closedir(d);
-      return true;
-    }
-    // not a directory
-    fs_print_error(path, "is_empty:opendir");
+    // any non-directory
+    closedir(d);
     return false;
+  }
+    // empty directory
+    closedir(d);
+    return true;
+  }
+
+  // not a directory
+  fs_print_error(path, "is_empty");
+  return false;
 
 #endif
-  }
-  else {
-    return fs_exists(path) && fs_file_size(path) == 0;
-  }
 
 #endif
 
