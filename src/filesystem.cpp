2ae4717e159fae069160b6ccb5b24f45df050a52
scivision <scivision@users.noreply.github.com>
2024-12-03 11:54:24 -0500

    remove: simplify

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,49 +1,13 @@
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#endif
-
 #if defined(HAVE_CXX_FILESYSTEM)
 #include <filesystem>
-#elif !defined(_WIN32)
-#include <cstdio> // remove()
+#else
+#include <cstdio> // for std::remove, std::rename
 #endif
 
 #include <string_view>
 #include <system_error>         // for error_code
 
 #include "ffilesystem.h"
-
-
-#if defined(_WIN32) && !defined(HAVE_CXX_FILESYSTEM)
-
-static bool
-fs_win32_remove(std::string_view path)
-{
-
-  // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-removedirectorya
-  // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-deletefilea
-
-  if(fs_is_dir(path)){
-    if(RemoveDirectoryA(path.data()))
-      return true;
-    fs_print_error(path, "remove: directory faileed");
-    return false;
-  } else if(fs_is_file(path)){
-    if(DeleteFileA(path.data()))
-      return true;
-    fs_print_error(path, "remove: file failed");
-    return false;
-  } else {
-    fs_print_error(path, "remove: is not a file or directory");
-    return false;
-  }
-
-  fs_print_error(path, "remove");
-  return false;
-}
-
-#endif
 
 
 bool
@@ -53,21 +17,21 @@ fs_remove(std::string_view path)
   std::error_code ec;
 
 #ifdef HAVE_CXX_FILESYSTEM
+  // https://en.cppreference.com/w/cpp/filesystem/remove
   if(std::filesystem::remove(path, ec) && !ec) FFS_LIKELY
-    return true;
-#elif defined(_WIN32)
-  return fs_win32_remove(path);
 #else
+  // https://en.cppreference.com/w/cpp/io/c/remove
   if(std::remove(path.data()) == 0)
-    return true;
 #endif
+    return true;
 
   fs_print_error(path, "remove", ec);
   return false;
 }
 
 
-bool fs_rename(std::string_view from, std::string_view to)
+bool
+fs_rename(std::string_view from, std::string_view to)
 {
   // rename a file or directory
   // existing non-empty "to" is an error
@@ -80,9 +44,11 @@ bool fs_rename(std::string_view from, std::string_view to)
   }
 
 #ifdef HAVE_CXX_FILESYSTEM
+  // https://en.cppreference.com/w/cpp/filesystem/rename
   std::filesystem::rename(from, to, ec);
   if(!ec) FFS_LIKELY
 #else
+  // https://en.cppreference.com/w/cpp/io/c/rename
   if(std::rename(from.data(), to.data()) == 0)
 #endif
     return true;
