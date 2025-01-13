a9fb9e00396b4c48accb0474736281a402435032
scivision <scivision@users.noreply.github.com>
2025-01-13 14:08:29 -0500

    test:is_appexec: use wt.exe

--- test/core/test_is_appexec_alias.cpp
+++ test/core/test_is_appexec_alias.cpp
@@ -16,27 +16,25 @@ if (!fs_is_windows()) {
   return 77;
 }
 
-std::string_view path;
+std::string path;
+// not string_view, it garbles on some platforms
 
 if (argc > 1)
   path = argv[1];
 else {
   const std::string appdir = fs_user_config_dir() + "/Microsoft";
   std::cout << "App Execution Alias directory: " << appdir << "\n";
-  // try a few exe like bash.exe, wt.exe
-  // notepad.exe is not always an alias, as it was a system program historically
-  for (const auto& p : {"bash.exe", "wt.exe", "code.cmd", "ninja.exe", "notepad.exe", "pbrush.exe"}) {
-    std::string s = fs_which(p);
-    if (!s.empty() && fs_is_subdir(s, appdir)) {
-      path = s;
-      break;
-    }
-  }
-}
 
-if(path.empty()){
-  std::cerr << "didn't find any App Execution Alias to test, skipping\n";
-  return 77;
+  std::string s = fs_which("wt.exe");
+  if (!s.empty() && fs_is_subdir(s, appdir))
+    path = s;
+
+  std::cout << "which: " << s << "\n";
+
+  if(path.empty()){
+    std::cerr << "didn't find any App Execution Alias to test, skipping\n";
+    return 77;
+  }
 }
 
 std::cout << "Testing fs_is_appexec_alias(" << path << ")\n";
